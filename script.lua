f21cf74fb44cabddb70f569ba3e7f34d57398614
evergales <97823982+evergales@users.noreply.github.com>
2026-08-07 15:19:39 +0200

    yis

--- script.lua
+++ script.lua
@@ -115,16 +115,3 @@ table.insert(patpat.onUnpat, function() -- if you dont specify if event is for p
   animations.gali.patted:stop()
   models.gali.root.Torso.Head.Face:setUVPixels(0, 0) -- unset the blushy face
 end)
-
--- stupidass create wonky cam fix
-if host:isHost() then
-  events.WORLD_RENDER:register(function(delta)
-    -- checks for freecam
-    -- checks if we're specifically sitting on a create seat
-    if not ((client.getCameraEntity() ~= player) or (world.getPlayers().FreeCamera ~= nil)) and player:getVehicle() and player:getVehicle():getType() == "create:seat" then
-      renderer:setCameraRot(((player:getRot(delta).xy_ + vec(0, renderer:isCameraBackwards() and 180 or 0, 0))) * vec(renderer:isCameraBackwards() and -1 or 1, 1, 1))
-    else
-      renderer:setCameraRot() -- remove any override if we're not seated or in freecam
-    end
-  end)
-end
